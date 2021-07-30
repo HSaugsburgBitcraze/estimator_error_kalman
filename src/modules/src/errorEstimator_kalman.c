41231b5adb0f3f11a6ad59f97c920af3da4e0b81
Klaus <klaus.kefferpuetz@hs-augsburg.de>
2021-07-30 18:49:46 +0200

    Landing Function and Sweep angle measurements integrated

--- src/modules/src/errorEstimator_kalman.c
+++ src/modules/src/errorEstimator_kalman.c
@@ -1,18 +1,18 @@
 /**
+*                                             __
+*    __  __ ____      ____  __  __ __________/ /_  __   _____________
+*   / /_/ / ___/____ / __ `/ / / /´__  / ___/ __ \/ /  / / ___/ __  /
+*  / __  (__  )/___// /_/ / /_/ / /_/ (__  ) /_/ / /__/ / /  / /_/ /
+* /_/ /_/____/      \__,_/_____/\__  /____/\____/______/_/   \__  /
+*                              /____/                       /____/
+* Crazyflie Project
+*
+* Copyright (C) 2019-2021 University of Applied Sciences Augsburg
+*
+*/
+/**
  * Authored by Klaus Kefferpütz (https://www.hs-augsburg.de/fmv/kefferpuetz-klaus.html), December 2020
  *
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, in version 3.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program. If not, see <http://www.gnu.org/licenses/>.
  *
  * ================================================================================================
  * Error State Kalman Filter Approach combining strapdown navigation and estimated state error
